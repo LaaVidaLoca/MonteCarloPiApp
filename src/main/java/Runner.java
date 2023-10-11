741efa2181094847c13f20271b6d065713fe84cb
Dmitry Tsipino <LaaVidaaLocaa@yandex.ru>
2023-10-11 22:33:15 +0300

    lab6

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,11 +1,7 @@
 public class Runner {
     public static void main(String[] args) {
         Thread thread = new Thread(() -> {
-            MonteCarloPiFinder monteCarloPiFinder = new MonteCarloPiFinder(500000000);
-            long start = System.currentTimeMillis();
-            double pi = monteCarloPiFinder.getPi();
-            long stop = System.currentTimeMillis();
-            System.out.println("Значение Pi: " + pi + ", время: " + (stop - start) + " мс.");
+
         });
         thread.start();
     }
